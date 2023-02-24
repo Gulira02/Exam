0d6db061063a9f1ee364b76ec8b53597fa8cb54b
user <gulira.abdukerimkyzy.jk@gmail.com>
2023-02-24 19:20:45 +0600

    addExam

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,56 @@
-package PACKAGE_NAME;public class Book {
+public class Book {
+    private String name;
+    private double price;
+    private Author author;
+    private  int qty=0;
+
+    public Book(String name, double price, Author author) {
+        this.name = name;
+        this.price = price;
+        this.author = author;
+        this.qty = qty;
+    }
+    public  Book(String kelkel, double v, String s, String s1, char m, int i){}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                ", author=" + author +
+                ", qty=" + qty +
+                '}';
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static <Books> void main(String[] args)
+    {
+        Book book1=new Book("Kelkel",922.34,new Author("Tologon Kasymbekov","tologon1@gmail.com",'m',123));
+        Book book2=new Book("Esine Kaitkan Rosa",726.34, new Author("Nurjigit","nurjigit@gmail.com",'m',1223));
+        Book book3=new Book("Sen",124.34, new Author("Serrary@mail.com","nur@gmail.com",'m',345));
+
+
+        Book[] books={book1,book2,book3};
+        for (Book book:books) {
+            System.out.println(book);
+        }
+
+
     }
 }
